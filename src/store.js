b8e818784ece97106adcab288a9d6cc9de355a54
fandok <fandika.okdiba@gmail.com>
2024-02-07 22:07:18 +0700

    komentar tentang devtools

--- src/store.js
+++ src/store.js
@@ -6,6 +6,8 @@ import combinedReducers from "./redux/reducer";
 const store = configureStore({
   // punya 1 parameter wajib yaitu reducer-nya itu sendiri
   reducer: combinedReducers,
+  // command buat munculin devtools atau ngga, biasanya true
+  // devTools: false,
 });
 
 export default store;
